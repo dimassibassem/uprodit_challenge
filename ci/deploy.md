abda4369bfca5469790cdb7e202604468384d8b0
Bassem <dimassibassem99@gmail.com>
2023-08-16 03:29:20 +0100

    :wrench: fixes

--- ci/deploy.md
+++
@@ -1,3 +0,0 @@
-# Force redeploy
-
-This markdown has only this purpose: allow to retrigger CI/CD without changing sources.
--- ci/mirror.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-REPO_PATH="${PROJECT_HOME}/uprodit_challenge/"
-
-cd "${REPO_PATH}" && git pull origin main || :
-git reset --hard origin/main
-git push -f github main
-exit 0

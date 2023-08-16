583ad7c36c615fd655b1ab4acad13c5083911621
Bassem <dimassibassem99@gmail.com>
2023-08-16 02:38:59 +0100

    :memo: Adding services

---
+++ src/services/search/index.ts
@@ -0,0 +1,21 @@
+import { client } from "../axios";
+import { apiEndpoints } from "../../config";
+import { SearchQuerySettings, User } from "./types";
+
+export async function search(
+  querySettings: SearchQuerySettings = { startIndex: 0, maxResults: 20 }
+): Promise<User[]> {
+  const endpoint = buildSearchEndpoint(querySettings);
+  const searchResponse = await client.get<User[]>(endpoint);
+  return searchResponse.data;
+}
+
+function buildSearchEndpoint(querySettings: SearchQuerySettings) {
+  let endpoint = `${apiEndpoints.search}`;
+
+  Object.entries(querySettings).forEach(
+    ([key, value]) => (endpoint += (key == "startIndex" || (key && value)) ? `&${key}=${value}` : '')
+  );
+
+  return endpoint;
+}

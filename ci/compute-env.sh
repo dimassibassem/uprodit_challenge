631b6b63d82f8c44dd9acbc6cc39e6c04d3f4b3a
Your Name <you@example.com>
2023-08-16 22:13:15 +0100

    Deleting CI folder

--- ci/compute-env.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-export VERSION="${CI_COMMIT_BRANCH}-${CI_COMMIT_SHORT_SHA}"
-export ENV="prod"
--- ci/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-source ./ci/compute-env.sh
-
-docker login "${CI_REGISTRY}" --username "${CI_REGISTRY_USER}" --password "${CI_REGISTRY_PASSWORD}"
-docker-compose -f "docker-compose-${ENV}.yml" up -d --force-recreate

d28a18c0f755a9d33475dc1aae9b0bfe74f37fb4
Bassem <dimassibassem99@gmail.com>
2023-08-16 14:26:53 +0100

    testing mirror

--- ci/mirror.sh
+++ ci/mirror.sh
@@ -2,7 +2,7 @@
 
 REPO_PATH="${PROJECT_HOME}/uprodit_challenge/"
 
-cd "${REPO_PATH}" && git pull origin main || :
-git reset --hard origin/main
-git push -f github main
+cd "${REPO_PATH}" && git pull origin master || :
+git reset --hard origin/master
+git push -f gitlab master
 exit 0

d2b79b7b5f983f03d473312a982ebe846f0ceb54
Bassem <dimassibassem99@gmail.com>
2023-08-16 14:42:45 +0100

    testing mirror

--- ci/mirror.sh
+++ ci/mirror.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-REPO_PATH="${PROJECT_HOME}/uprodit_challenge/"
+GITLAB_REMOTE_NAME="gitlab"
+BRANCH_NAME="master"
 
-cd "${REPO_PATH}" && git pull origin master || :
-git reset --hard origin/master
-git push -f gitlab master
+cd $GITHUB_WORKSPACE
+git pull origin $BRANCH_NAME
+git reset --hard origin/$BRANCH_NAME
+git push -f $GITLAB_REMOTE_NAME $BRANCH_NAME
 exit 0
